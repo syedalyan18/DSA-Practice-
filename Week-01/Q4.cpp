21a721a253a9023b92ba33529e6d9cc83113c919
0648-Alyan <k250648@nu.edu.pk>
2026-08-08 21:34:26 +0500

    First Commit

---
+++ Week-01/Q4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+int arr[]={1,2,3,4,5};
+
+cout<< arr<<endl;
+cout<< arr + 1<<endl;
+cout<< *(arr)<<endl;
+cout<< *(arr + 4)<<endl;
+
+}
